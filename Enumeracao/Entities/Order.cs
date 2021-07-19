cf9fdd6b71751486d1837a3be3b640a6589a0c76
Daniel <daniel.gdc22@gmail.com>
2021-07-19 17:38:48 -0300

    Introducao a ENUMS dentro de c#

---
+++ Enumeracao/Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Enumeracao.Entities.Enums;
+
+namespace Enumeracao.Entities
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Pedido{ get; set; }
+
+
+        public override string ToString()
+        {
+            return ID + ", " + Moment + " , " + Pedido;
+        }
+    }
+}
---
+++ Enumeracao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Enumeracao.Entities.Enums;
+using Enumeracao.Entities;
+
+
+namespace Enumeracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                ID = 1080,
+                Moment = DateTime.Now,
+                Pedido = OrderStatus.PaymentPending
+
+            };
+
+
+            Console.WriteLine(order);
+
+
+            /* Exemplos de conversao da entrada de dados em string para o tipo ENUMS */
+
+            string txt = OrderStatus.PaymentPending.ToString(); /*Converte o Tipo enums para STRING */
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); /* Enum.Parse <> , dentro de <> vai o Tipo de dado a ser convertido e em () o status deve ser instruido da mesma forma que a declaracao em Ordestatus*/
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+
+        }
+    }
+}
