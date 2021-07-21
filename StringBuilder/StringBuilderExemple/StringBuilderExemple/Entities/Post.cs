57f941282d281f508b8cbcd53e3c50fb25df155f
Daniel <daniel.gdc22@gmail.com>
2021-07-21 14:26:04 -0300

    Utilizacao de Stribuilder e suas atribuicoes

---
+++ StringBuilder/StringBuilderExemple/StringBuilderExemple/Entities/Post.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using StringBuilderExemple.Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBuilderExemple.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public string Title { get; set; }
+
+        public List<Comment> Comment { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+
+        }
+
+        public Post(DateTime moment, string content, int likes, string title)
+        {
+            Moment = moment;
+            Content = content;
+            Likes = likes;
+            Title = title;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comment.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comment.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(Title);
+            sb.Append(Likes );
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyy HH:mm:ss"));
+            sb.AppendLine("Comments:");
+            foreach(Comment c in Comment)
+            {
+                sb.AppendLine(c.Text);
+            }
+
+            return sb.ToString();
+
+
+           
+
+        }
+
+
+    }
+}
---
+++ StringBuilder/StringBuilderExemple/StringBuilderExemple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using StringBuilderExemple.Entities;
+
+namespace StringBuilderExemple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("What a awesome notice!!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:45:56"),
+                "Traveling across the world",
+                165,
+                "Im going to Brazil right now"
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+        }
+    }
+}
